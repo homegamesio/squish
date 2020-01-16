6bc6b9b6b6ba4f26ffdf8d88fc0681556dec45b0
Joseph Garcia <prosif@gmail.com>
2020-01-15 20:16:43 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const { squish, unsquish } = require('./src/squish');
+const GameNode = require('./src/GameNode');
+const Colors = require('./src/Colors');
+
+module.exports = {
+    squish,
+    unsquish,
+    GameNode,
+    Colors
+};
