534b91e76f19e7ce945af1b9dc7f6e17dd30374e
Joseph Garcia <prosif@gmail.com>
2020-04-03 20:15:19 -0700

    add handleclick + effects squish stuff

--- src/squish.js
+++ src/squish.js
@@ -10,6 +10,8 @@ const POS_SUBTYPE = 45;
 const SIZE_SUBTYPE = 46;
 const TEXT_SUBTYPE = 47;
 const ASSET_SUBTYPE = 48;
+const EFFECTS_SUBTYPE = 49;
+const ONCLICK_SUBTYPE = 50;
 
 const squishSpec = {
     id: {
@@ -143,6 +145,63 @@ const squishSpec = {
                 }
             }
         }
+    },
+    effects: {
+        type: EFFECTS_SUBTYPE,
+        squish: (a) => {
+            if (a['shadow']) {
+                const assetKey = 'shadow';
+                let squishedLength = assetKey.length + 4; // + 4 for color
+                if (a['shadow'].blur) {
+                    squishedLength += 2;
+                }
+                const squishedEffects = new Array(squishedLength);
+                for (let i = 0; i < assetKey.length; i++) {
+                    squishedEffects[i] = assetKey.charCodeAt(i);
+                }
+                squishedEffects[assetKey.length] = a.shadow.color[0];
+                squishedEffects[assetKey.length + 1] = a.shadow.color[1];
+                squishedEffects[assetKey.length + 2] = a.shadow.color[2];
+                squishedEffects[assetKey.length + 3] = a.shadow.color[3];
+
+                if (a.shadow.blur) {
+                    squishedEffects[assetKey.length + 4] = Math.floor(a.shadow.blur / 10)
+                    squishedEffects[assetKey.length + 5] = a.shadow.blur % 10
+                }
+
+                return squishedEffects;
+            }
+        },
+        unsquish: (squished) => {
+            // 'shadow' is all (for now)
+            const assetKey = String.fromCharCode.apply(null, squished.slice(0, 6));
+            const color = squished.slice(6, 10);
+            let blur;
+            if (squished.length > 10) {
+                blur = squished[10] * 10 + squished[11];
+            }
+
+            const unsquished = {
+                [assetKey]: {
+                    color
+                }
+            };
+
+            if (blur) {
+                unsquished[assetKey].blur = blur;
+            }
+
+            return unsquished;
+        }
+    },
+    handleClick: {
+        type: ONCLICK_SUBTYPE,
+        squish: (a) => {
+            return a ? [1] : [0];
+        },
+        unsquish: (a) => {
+            return a[0] === 1;
+        }
     }
 };
 
@@ -153,7 +212,9 @@ const squishSpecKeys = [
     'pos', 
     'size', 
     'text', 
-    'asset' 
+    'asset',
+    'effects',
+    'handleClick'
 ];
 
 const typeToSquishMap = {};
