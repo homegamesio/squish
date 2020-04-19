b7e07551b006e063f0b82dec5f9dc4407a02f68f
Joseph Garcia <prosif@gmail.com>
2020-04-19 12:59:05 -0700

    move Game.js to squish

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { squish, unsquish } = require('./src/squish');
 const GameNode = require('./src/GameNode');
 const Colors = require('./src/Colors');
+const Game = require('./src/Game');
 
 module.exports = {
     squish,
     unsquish,
+    Game,
     GameNode,
     Colors
 };
