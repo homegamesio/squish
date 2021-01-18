7ebe4c35d1f8e6eef157b5710d471884c5131287
Joseph Garcia <prosif@gmail.com>
2021-01-18 01:00:32 -0700

    new test log

--- src/squish.js
+++ src/squish.js
@@ -364,7 +364,7 @@ const unsquish = (squished) => {
     }
 
 const squish = (entity, scale = null) => {
-    console.log('i am squish 0.6.31');
+    console.log('i am squish 0.6.32');
     let squishedPieces = [];
 
     for (const keyIndex in squishSpecKeys) {
