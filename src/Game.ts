2526419e2b69366a85f57aa0229c2b256ccbff29
YazeedLoonat <yazeedloonat@gmail.com>
2021-08-15 22:35:13 -0700

    Converting everything to typescript

--- src/Game.ts
+++ src/Game.ts
@@ -1,4 +1,14 @@
-class Game {
+import { playerDef, playersDef, gameDef } from './sharedDefs';
+
+type timedFunctionDef = () => void;
+
+export default class Game implements gameDef {
+    players: playersDef;
+    timeouts: NodeJS.Timeout[];
+    intervals: NodeJS.Timer[];
+    listeners: any;
+    root: any;
+
     constructor() {
         this.players = {};
         this.listeners = new Set();
@@ -7,19 +17,19 @@ class Game {
         this.timeouts = [];
     }
 
-    _hgAddPlayer(player) {
+    _hgAddPlayer(player: playerDef) {
         this.players[player.id] = player;
     }
 
-    _hgRemovePlayer(playerId) {
+    _hgRemovePlayer(playerId: number) {
         delete this.players[playerId];
     }
 
-    addStateListener(listener) {
+    addStateListener(listener: any) {
         this.listeners.add(listener);
     }
 
-    removeStateListener(listener) {
+    removeStateListener(listener: any) {
         this.listeners.remove(listener);
     }
 
@@ -27,13 +37,13 @@ class Game {
         return this.root;
     }
 
-    setInterval(fun, interval) {
+    setInterval(fun: timedFunctionDef, interval: number) {
         const ticker = setInterval(fun, interval);
         this.intervals.push(ticker);
         return ticker;
     }
 
-    setTimeout(fun, time) {
+    setTimeout(fun: timedFunctionDef, time: number) {
         const timeout = setTimeout(fun, time);
         this.timeouts.push(timeout);
         return timeout;
@@ -43,7 +53,7 @@ class Game {
         for (const i in this.timeouts) {
             const timeout = this.timeouts[i];
             clearTimeout(timeout);
-        } 
+        }
 
         for (const i in this.intervals) {
             const interval = this.intervals[i];
@@ -51,6 +61,3 @@ class Game {
         }
     }
 }
-
-module.exports = Game;
-
