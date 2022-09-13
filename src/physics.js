9b1dad3cd8d774a8bad75d49e80cd2272b634ef8
Joseph Garcia <prosif@gmail.com>
2022-09-12 20:10:43 -0700

    include last point

--- src/physics.js
+++ src/physics.js
@@ -10,6 +10,24 @@ const Physics = {
             const newY = curY + yVel;
             
             if (newX < 0 || newX > endX || newY < 0 || newY > endY) {
+                let _newX = newX;
+                let _newY = newY;
+                if (newX < 0) {
+                    _newX = 0;
+                } else if (newX > endX) {
+                    _newX = endX;
+                }
+
+                if (newY < 0) {
+                    _newY = 0;
+                } else if (newY > endY) {
+                    _newY = endY;
+                }
+
+                if (!(path[path.length - 1][0] === _newX || path[path.length - 1][1] === _newY)) {
+                    path.push([_newX, _newY]);
+                }
+
                 withinEdges = false;
             } else {
                 path.push([newX, newY]);
