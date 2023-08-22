119fef4c0b0e6053949190c3b0f1c8bcb8f6584a
Joseph Garcia <prosif@gmail.com>
2023-08-22 16:30:31 -0700

    1.0.3

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,19 @@
 const path = require('path');
 
 const getAppDataPath = () => {
+  if (!process) {
+    return "";
+  }
+
   switch (process.platform) {
     case "darwin": {
-      return path.join(process.env.HOME, "Library", "Application Support", "Your app name");
+      return path.join(process.env.HOME, "Library", "Application Support", "homegames");
     }
     case "win32": {
-      return path.join(process.env.APPDATA, "Your app name");
+      return path.join(process.env.APPDATA, "homegames");
     }
     case "linux": {
-      return path.join(process.env.HOME, ".Your app name");
+      return path.join(process.env.HOME, ".homegames");
     }
     default: {
       console.log("Unsupported platform!");
@@ -17,29 +21,6 @@ const getAppDataPath = () => {
     }
   }
 }
-
-
-function saveAppData(content) {
-    const appDatatDirPath = getAppDataPath();
-    
-    // Create appDataDir if not exist
-    if (!fs.existsSync(appDatatDirPath)) {
-        fs.mkdirSync(appDatatDirPath);
-    }
-
-    const appDataFilePath = path.join(appDatatDirPath, 'appData.json');
-    content = JSON.stringify(content);
-
-    fs.writeFile(appDataFilePath, content, (err) => {
-        if (err) {
-            console.log("There was a problem saving data!");
-            // console.log(err);
-        } else {
-            console.log("Data saved correctly!");
-            loadAppData();
-        }
-    });
-};
 
 module.exports = {
     getAppDataPath
