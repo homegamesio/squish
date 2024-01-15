921d8570c6d36bd89fc58896af204f955fde0eeb
Joseph Garcia <prosif@gmail.com>
2024-01-14 17:46:04 -0700

    wat lol

--- src/util/index.ts
+++
@@ -1,6 +0,0 @@
-export const hypLength = (x: number, y: number) => Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-
-// get the first 2 digits after the decimal
-export const getFractional = (number: number) => {
-    return Math.round(100 * (number - Math.floor(number)));
-};
